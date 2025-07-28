16b0f180cc56632604007fddcfd77efee20b904d
nadula14 <madugalle_ewmwwndb_e22@engug.ruh.ac.lk>
2025-07-29 01:22:46 +0530

    second

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+     <div>2025 Madmax.All rights reserved</div>
+     <div></div>
+
+    </div>
+  )
+}
+
+export default Footer
