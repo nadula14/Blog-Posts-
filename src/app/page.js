cb67a8058d5b461e9c431d77a6661dcddda21c34
nadula14 <madugalle_ewmwwndb_e22@engug.ruh.ac.lk>
2025-08-03 01:02:46 +0530

    third

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,28 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+//import Hero from 'public/hero.png';
 
 export default function Home() {
   return (
-    <div>
-      Hello HI
+    <div className={styles.container}>
+    <div className={styles.item} >
+     <h1 className={styles.title}>Better Design For Your Digital Products
+       </h1>
+      <p className={styles.desc}>Turning your Idea into Reality. We bring together the teams from the
+          global tech industry.
+      </p>
+      <button className={styles.button}>See Our Work</button>
+      </div>   
+
+    <div className={styles.imageWrapper}>
+    <Image src="/hero.png" 
+    className={styles.img}
+    style={{objectFit : "cover"}} 
+    alt=""
+    priority
+    fill
+       />
+    </div>
     </div>
   );
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import styles from './footer.module.css'
+import Image from 'next/image'
 
 const Footer = () => {
   return (
     <div className={styles.container}>
      <div>2025 Madmax.All rights reserved</div>
-     <div></div>
+     <div className={styles.social}>
+      <Image src ="/1.png"  width = {15} height = {15} className={styles.icon}  alt= "hello" />
+      <Image src ="/2.png"  width = {15} height = {15} className={styles.icon} alt= "hello" />
+      <Image src ="/3.png"  width = {15} height = {15} className={styles.icon} alt= "hello" />
+      <Image src ="/4.png"  width = {15} height = {15} className={styles.icon} alt= "hello" />
+
+
+
+     </div>
 
     </div>
   )
